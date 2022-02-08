7129c09bb38b70dc16f6ebfd5661f339c1ebae86
Yan Cardoso <yancard7@gmail.com>
2022-02-08 18:14:26 -0300

    word

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
 const messageDisplay = document.querySelector('.message-container')
 
-let wordle = 'CORNO'
+let wordle = 'HAVIA'
 
 // const getWordle = () => {
 //     fetch('http://localhost:8000/word')
